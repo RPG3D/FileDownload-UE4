29699cf9727d54561fac461a9dcfd96d38637901
RPG3D <imxqliu@live.com>
2017-12-28 23:16:45 +0800

    1. add VS project

---
+++ DownloadExample/Source/DownloadExample/GenerateVSPeoject.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GenerateVSPeoject.h"
+
+
+// Sets default values
+AGenerateVSPeoject::AGenerateVSPeoject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGenerateVSPeoject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGenerateVSPeoject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
